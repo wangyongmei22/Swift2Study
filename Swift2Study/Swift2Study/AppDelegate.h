461da0b5fd6a70c0807ab4a70effeef29c8f11e5
wangyongmei <wangyongmei@example.com>
2017-12-07 17:22:29 +0800

    dd

---
+++ Swift2Study/Swift2Study/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Swift2Study
+//
+//  Created by 王永梅 on 2017/12/7.
+//  Copyright © 2017年 sitech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
